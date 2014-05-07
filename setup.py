75275872bfa00508d54b46b572ce03ea81c9c6f7
Zhang Ping <dqzhangp@163.com>
2014-05-07 09:33:12 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 
 from setuptools import setup, find_packages
 
-extractors = [
-    ('/m3u8player/public/**', 'ignore', None),
-]
-
 setup(
-      name='M3U8Player',
-      version='1.0',
+    name='M3U8Player',
+    version='1.0',
 
-      author='Zhang Ping',
-      author_email='dqzhangp@163.com',
-      license='GPL v3 or later',
-        
-      packages=find_packages(),
-      entry_points = {
-          'mediacore.plugin': [
-              'm3u8player = m3u8player.mediacore_plugin',
-          ],
-      }
+    author='Zhang Ping',
+    author_email='dqzhangp@163.com',
+    license='GPL v3 or later',
+
+    packages=find_packages(),
+    entry_points = {
+        'mediacore.plugin': [
+            'm3u8player = m3u8player.mediacore_plugin',
+        ],
+    }
 )
 
