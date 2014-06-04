feccfe0686e02222af92737fe14b4724e2ea35fe
Zhang Ping <dqzhangp@163.com>
2014-06-04 07:54:40 -0400

    setup

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@ from setuptools import setup, find_packages
 
 setup(
     name='M3U8Player',
-    version='1.0',
-
+    version='0.1.0',
     author='Zhang Ping',
     author_email='dqzhangp@163.com',
-    license='GPL v3 or later',
+    description='A MediaCore plugin m3u8 player.',
 
     packages=find_packages(),
-
-    package_data = {
-        'm3u8player': ['public/scripts/third-party/*', 'templates/*'],
-    },
+    namespace_packages = ['m3u8player'],
+    include_package_data=True,
+    zip_safe = False,
 
     entry_points = {
         'mediacore.plugin': [
             'm3u8player = m3u8player.mediacore_plugin',
         ],
-    }
+    },
+
+    message_extractors = {'m3u8player/': [
+        ('**.py', 'python', None),
+        ('templates/**.html', 'genshi', {'template_class': 'genshi.template.markup:MarkupTemplate'}),
+        ('public/**', 'ignore', None),
+    ]},
 )
 
