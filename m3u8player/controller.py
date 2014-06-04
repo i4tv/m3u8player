05701e124192b48ae81a448350ada6c4df9254f9
Zhang Ping <dqzhangp@163.com>
2014-06-04 15:15:49 +0800

    controller

--- m3u8player/controller.py
+++ m3u8player/controller.py
@@ -4,7 +4,7 @@ from mediacore.lib.base import BaseController
 from mediacore.lib.decorators import expose
 
 class M3U8Controller(BaseController):
-    @expose('m3u8player/m3u8_page.html')
+    @expose('m3u8player/m3u8player_page.html')
     def m3u8_page(self, tag_name, **kwargs):
         # do your backend work here
         # …
