d80dd58b3aeeb005210b29a09d20bb9dfd8bd36f
Zhang Ping <dqzhangp@163.com>
2014-06-07 20:51:17 -0400

    supported containers and schemes

--- m3u8player/player.py
+++ m3u8player/player.py
@@ -7,13 +7,14 @@ class M3U8Player(AbstractFlashPlayer):
     name = u'm3u8player'
     display_name = u'M3U8 Player'
 
-    # you could override flashvars() for example to add player some options
+    supported_containers = set(['m3u8'])
+    supported_schemes = set(['http'])
 
     def swf_url(self):
         """Return the flash player URL."""
         return url_for('/m3u8player/public/scripts/third-party/HLSProviderChromeless.swf', qualified=self.qualified)
 
-
+    # you could override flashvars() for example to add player some options
     def flashvars(self):
         """Return a python dict of flashvars for this player."""
         return {}
